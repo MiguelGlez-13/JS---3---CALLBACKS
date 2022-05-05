be265e47c4b7bb2a1f7d29c4476a1b702319be5b
MiguelGlez-13 <miguelglez20xx@hotmail.com>
2022-05-06 00:59:23 +0100

    cCeacion de ficheros

---
+++ script.js
@@ -0,0 +1,10 @@
+//CallBacks
+
+//Function - Forma 1
+document.getElementById("button01").addEventListener("click", function () {
+    console.log("has pulsado el botón 1");
+});
+//Arrow function - Forma 2
+document.getElementById("button02").addEventListener("click", () => {
+console.log("has pulsado el botón 2");
+});
