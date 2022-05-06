22c15a556bd142bfe98c16ffdbfc8d3d42fb850f
MiguelGlez-13 <miguelglez20xx@hotmail.com>
2022-05-06 17:48:45 +0100

    final de clase! - revisar funcionamiento loader

--- loaders.js
+++ loaders.js
@@ -1,20 +1,27 @@
 const loader = document .getElementById ('lds-spinner')
 
-let promesa3 = new Promise((resolve, reject) => {
-    setTimeout (() => {
-        resolve ("OK");
-}, 3000);
+console.log("Inicio del JS")
+
+let promesa = new Promise((resolve, reject) => {
+    console.log("Inicio del TimeOut")
+    setTimeout(() => {
+        console.log("Final TimeOut")
+//resolve("OK");
+// o
+    reject(new Error("Ocurrió un error"));
+}, 2000);
 });
 
-
-loader.style.display = 'block';
-promesa3
+console.log("Ejecución de la Promesa")
+promesa
     .then((data) => {
-        console .log(data);
-})
-    .catch((err) => console .log(err.message ))
-    .finally (function () {
+        console.log(data);
     })
+    .catch((err) => console.log(err.message))
+    .finally(function () {
+        console.log('Ejecuto siempre. Por ejemplo ocultariamos un loader!!!')
+    })
+console.log("Termina la Promesa")
 
 
-//loader.style.display = 'none';
+//loader.style.display = 'none'; - solo se mustra mencionando esta línea - revisar
